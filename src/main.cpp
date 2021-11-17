5a494db9fcc2549ea8a8a530c8304ac10747a324
shawnmiller124 <83323320+shawnmiller124@users.noreply.github.com>
2021-11-17 14:10:43 -0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -20,28 +20,20 @@
 #define WHEEL_BASE_SPEED 0.25
 #define WHEEL_ADD_SPEED 0.05
 
-#define FOLLOW_LINE_TIMER 1
-#define DETECT_BOWLING_PIN_TIMER 2
-#define DETECT_SOUND_TIMER 3
+
+#define TIMER_LINE_FALLOWER 3
+Adafruit_TCS34725 capteur = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_700MS, TCS34725_GAIN_1X); // création de l'objet capteur
 
 // OBP-704 possible values for suiveur de ligne
-#define LINE_LEFT 1.02    // 1 0 0
-#define LINE_RIGHT 1.90   // 0 0 1
-#define LINE_COLOUR 0.57  // 0 1 0
-#define LINE_COLOUR2 2.42 // 0 1 1
-#define LINE_COLOUR3 1.50 // 1 1 0
-#define LINE_COLOUR4 3.34 // 1 1 1
-
-#define LINE_UNDECIDED 2.88
+#define LINE_CENTER 1.45   // 1 0 0
+#define LINE_RIGHT 2.80  // 0 0 1
+#define LINE_LEFT 0.79 // 0 1 0
 
 // ANALOGS
 #define ANALOG_LINE_FOLLOWER A0
-#define ANALOG_BUZZER A1
+
 // DIGITALS
-#define DIGITAL_RED_LED 46
-#define DIGITAL_GREEN_LED 44
-#define DIGITAL_BLUE_LED 48
-#define DIGITAL_YELLOW_LED 42
+
 
 //WHEEL
 #define ENCODER_STEP 3200
@@ -77,430 +69,140 @@ int distanceEncodeur;
 float motorLeft = 0;
 float motorRight = 0;
 
-//TIMER0
-volatile uint8_t compt = 0;
-volatile bool encoderEqual = false;
+int angleMoteur;
 
-//ETAPE PARCOURS
-#define NBR_DISTANCE 2
-#define NBR_DISTANCEr 1
-#define NBR_DISTANCErr 2
-#define NBR_DISTANCEj 3
-#define NBR_DISTANCEjr 4
-#define NBR_DISTANCEb 3
-#define NBR_DISTANCEbr 4
-#define NBR_ANGLE 2
-#define NBR_ANGLEr 1
-#define NBR_ANGLErr 2
-#define NBR_ANGLEj 3
-#define NBR_ANGLEjr 4
-#define NBR_ANGLEb 3
-#define NBR_ANGLEbr 4
-float tabDistance[NBR_DISTANCE] = {0, 45};
-float tabDistancer[NBR_DISTANCEr] = {220};
-float tabDistancerr[NBR_DISTANCErr] = {0, 220};
-float tabDistancej[NBR_DISTANCEj] = {0, 40, 220};
-float tabDistancejr[NBR_DISTANCEjr] = {0, 220, 40, 75};
-float tabDistanceb[NBR_DISTANCEb] = {0, 40, 220};
-float tabDistancebr[NBR_DISTANCEbr] = {0, 220, 40, 75};
-int tabAngle[NBR_ANGLE] = {180, 0};
-int tabAngler[NBR_ANGLEr] = {0};
-int tabAnglerr[NBR_ANGLErr] = {180, 0};
-int tabAnglej[NBR_ANGLEj] = {-90, 90, 0};
-int tabAnglejr[NBR_ANGLEjr] = {180, -90, 90, 0};
-int tabAngleb[NBR_ANGLEbr] = {90, -90, 0};
-int tabAnglebr[NBR_ANGLEbr] = {180, 90, -90, 0};
-int tmpValueTab;
-bool etapeCouleur = false;
+typedef enum _step
+{
 
-bool executionStepParcours(uint16_t distance, int angle);
+} _step;
+_step Step;
+
+//definition fonction
+
+void executeStep();
+
+//timer function
+void timerInit(uint8_t id, void (*func)(), unsigned long delay, int32_t nrep);
+void timerDetectionLineFallower();
+volatile float voltageValue;
+
+//motor function
 void stopMotor();
 void movingFowardRobot(uint16_t distance);
 void turnedRobot(int angle);
-void reverseTab(int *tab, uint16_t sizeTab, bool sens);
-float accelerationDecelerationPID(float pourcentageVitesse, uint8_t acceleration, uint8_t deceleration,
-                                  float maxSpeed, float speed);
+float accelerationDecelerationPID(float pourcentageVitesse, uint8_t acceleration, uint8_t deceleration, float maxSpeed, float speed);
 
-int step = 1;
-int angleMoteur;
-//capteur couleur
-Adafruit_TCS34725 capteur = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_700MS, TCS34725_GAIN_1X); // création de l'objet capteur
-void porterBalle();
-void followLine();
-void detectBowlingPin();
-float getDistanceToBowlingPin();
-float getAngleToBowlingPin();
-void listenToSound();
-void findLineAgain();
-int detectColor();
-void bringToRightColour();
-void stopMotor();
 void servoMoteur(int angle);
-/* void init_Detection5kHz();*/
-void detectSound();
-void lightLED(bool red, bool green, bool blue, bool yellow);
-void initLEDsPin();
-void turnToBeInLine(bool sens);
+int detectColor();
+
 
 void setup()
 {
   BoardInit();
-  initLEDsPin();
+
+  delay(2000);
+
+  timerInit(TIMER_LINE_FALLOWER, timerDetectionLineFallower, 2, -1);
+
+
   readEncoder0 = 0;
   readEncoder1 = 0;
-  /* SERVO_Enable(0);
-  SERVO_Enable(1);
-  servoMoteur(150); */
-  SOFT_TIMER_SetCallback(FOLLOW_LINE_TIMER, followLine);
-  SOFT_TIMER_SetCallback(DETECT_BOWLING_PIN_TIMER, detectBowlingPin);
-  SOFT_TIMER_SetCallback(DETECT_SOUND_TIMER, detectSound);
+  servoMoteur(150);
 
-  //Ajout du temps de répétition
-  //SOFT_TIMER_SetDelay(int id, int ms)
-  SOFT_TIMER_SetDelay(FOLLOW_LINE_TIMER, 2);
-  SOFT_TIMER_SetDelay(DETECT_BOWLING_PIN_TIMER, 150);
-  SOFT_TIMER_SetDelay(DETECT_SOUND_TIMER, 50);
+  //Step = FALLOW_LINE1;
 
-  //Ajout du nombre de fois qu'il répétera la fonction (-1 = infini);
-  //SOFT_TIMER_SetRepetition(int id, int nbFois)
-  SOFT_TIMER_SetRepetition(FOLLOW_LINE_TIMER, -1);
-  SOFT_TIMER_SetRepetition(DETECT_BOWLING_PIN_TIMER, -1);
-  SOFT_TIMER_SetRepetition(DETECT_SOUND_TIMER, -1);
-
-  //détermine si le timer pour la fonction et activé ou désactivé.
-  //SOFT_TIMER_Enable(int id);
-  //SOFT_TIMER_disable(int id);
-  /* SOFT_TIMER_Enable(FOLLOW_LINE_TIMER); */
-  /* SOFT_TIMER_Enable(DETECT_BOWLING_PIN_TIMER); */
-  SOFT_TIMER_Enable(DETECT_SOUND_TIMER);
-  
-  lightLED(1, 0, 0, 0);
-  delay(500);
-  lightLED(1, 0, 1, 0);
-  delay(500);
-  lightLED(1, 0, 1, 1);
+  SOFT_TIMER_Enable(TIMER_LINE_FALLOWER); //active suiveur de ligne
 
 }
 
 void loop()
 {
   SOFT_TIMER_Update();
+
+  executeStep();
 }
 
-void followLine()
+void executeStep()
 {
-  // Ajustement des roues pour le suiveur de ligne
-  // Vue qu'on se base sur les capteurs, nous n'utilisons pas de PID :)
-  //on va chercher le voltage
-  //on ajuste les roues dépendant du voltage
-  float voltageValue = (analogRead(ANALOG_LINE_FOLLOWER)) * (5 / 1023.0);
+
+  /*switch (Step)
+  {
+  case FALLOW_LINE1:
+
+    break;
+
+  case FIND_PIN:
+
+    break;
+
+  case TURN_TO_PIN:
+
+    break;
+
+  case GO_TO_PIN:
+
+    break;
+
+  case TURN_RIGHT:
+
+    break;
+
+  case FALLOW_LINE_COLOR:
+
+    break;
+
+  case TURN_LEFT:
+
+    break;
+
+  case FALLOW_LINE2:
+
+    break;
+
+  case DETECT_COLOR:
+
+    break;
+
+  case PICKUP_BALL:
+
+    break;
+
+  case RED_PATH:
+
+    break;
+
+  case YELLOW_PATH:
+
+    break;
+
+  case BLUE_PATH:
+
+    break;
+
+  case FIND_WHITE_LINE:
+
+    break;
+  }*/
+}
+
+void timerInit(uint8_t id, void (*func)(), unsigned long delay, int32_t nrep)
+{
+
+  SOFT_TIMER_SetCallback(id, func);
+  SOFT_TIMER_SetDelay(id, delay);
+  SOFT_TIMER_SetRepetition(id, nrep);
+}
+
+void timerDetectionLineFallower()
+{
+  voltageValue = (analogRead(ANALOG_LINE_FOLLOWER)) * (5 / 1023.0);
   Serial.println(voltageValue);
-  float motor_left = 0;
-  float motor_right = 0;
-  float motor_base_value = 0.7;
-  if (etapeCouleur && ((
-    (voltageValue >= LINE_COLOUR - 0.05 && voltageValue <= LINE_COLOUR + 0.05) ||
-    (voltageValue >= LINE_COLOUR2 - 0.05 && voltageValue <= LINE_COLOUR2 + 0.05) ||
-    (voltageValue >= LINE_COLOUR3 - 0.05 && voltageValue <= LINE_COLOUR3 + 0.05) ||
-    (voltageValue >= LINE_COLOUR4 - 0.05 && voltageValue <= LINE_COLOUR4 + 0.05)))){
-      SOFT_TIMER_Disable(FOLLOW_LINE_TIMER);
-      stopMotor();
-      delay(500);
-      turnedRobot(45);
-      turnToBeInLine(true);
-
-  }
-  else if ((voltageValue >= LINE_RIGHT - 0.05 && voltageValue <= LINE_RIGHT + 0.05) || (voltageValue >= LINE_UNDECIDED - 0.05 && voltageValue <= LINE_UNDECIDED + 0.05))
-  {
-    motor_right = motor_base_value;
-    motor_left = -motor_base_value;
-  }
-  else if (voltageValue >= LINE_LEFT - 0.05 && voltageValue <= LINE_LEFT + 0.05)
-  {
-    motor_left = motor_base_value;
-    motor_right = -motor_base_value;
-  }
-  else
-  {
-    motor_left = motor_base_value / 2;
-    motor_right = motor_base_value / 2;
-  }
-
-  MOTOR_SetSpeed(0, -motor_left);
-  MOTOR_SetSpeed(1, -motor_right);
-}
-
-void detectBowlingPin()
-{
-  //Mesurer avec l'infrarouge ou le sonar
-  float distance = SONAR_GetRange(1);
-  Serial.println(distance);
-  if (distance < 50.00 && distance > 0)
-  {
-    SOFT_TIMER_Disable(FOLLOW_LINE_TIMER);
-    stopMotor();
-    lightLED(1, 1, 1, 1);
-
-    turnedRobot(-90);
-    delay(500);
-    /* SOFT_TIMER_Enable(FOLLOW_LINE_TIMER); */
-
-    /* findLineAgain(); */
-    //tourner vers la droite
-    //CONTINUER D'AVANCER JUSQU'À
-    //V
-    //retrouve la linge
-    //recommence FOLLOW_LINE_TIMER
-    //TROUVER INTERSECTION
-    //DONNE LE CODE A ALEX
-
-    SOFT_TIMER_Disable(DETECT_BOWLING_PIN_TIMER);
-  }
-  //Prend en note à chaques fois qu'elle le détecte
-}
-
-void findLineAgain()
-{
-  delay(1000);
-  readEncoder0 = 0;
-  readEncoder1 = 0;
-  motorLeft = 0.35;
-  motorRight = 0;
-  sumError = 0;
-
-  ENCODER_Reset(RIGHT_WHEEL);
-  ENCODER_Reset(LEFT_WHEEL);
-  float voltageValue = 0;
-  Serial.println(voltageValue);
-  while (voltageValue < 1)
-  {
-    voltageValue = analogRead(ANALOG_LINE_FOLLOWER) * (5 / 1023.0);
-    Serial.println(voltageValue);
-    readEncoder0 = ENCODER_Read(LEFT_WHEEL);
-    readEncoder1 = ENCODER_Read(RIGHT_WHEEL);
-
-    //PID FOR ENCODER
-    float error = abs(readEncoder0 - readEncoder1);
-    float p = error * KP_FOWARD;
-
-    sumError += error;
-    float i = sumError * KI_FOWARD;
-
-    float adjSpeed = p + i;
-    motorRight = motorLeft + adjSpeed;
-
-    MOTOR_SetSpeed(LEFT_WHEEL, -motorLeft);
-    MOTOR_SetSpeed(RIGHT_WHEEL, -motorRight);
-  }
-  stopMotor();
-  turnToBeInLine(false);
-  SOFT_TIMER_Enable(FOLLOW_LINE_TIMER);
-  etapeCouleur = true;
-}
-
-void turnToBeInLine(bool sens){
-  float modif = 1;
-  if(sens){
-    modif = -1;
-  }
-
-  float voltageValue = 0;
-  bool leftFound = false;
-  bool rightFound = false;
-  MOTOR_SetSpeed(LEFT_WHEEL, 200*modif);
-  MOTOR_SetSpeed(RIGHT_WHEEL, -200*modif);
-  while (!(leftFound && rightFound))
-  {
-    voltageValue = analogRead(ANALOG_LINE_FOLLOWER) * (5 / 1023.0);
-    if (voltageValue >= LINE_RIGHT - 0.05 && voltageValue <= LINE_RIGHT + 0.05)
-    {
-      leftFound = true;
-    }
-    else if (voltageValue >= LINE_LEFT - 0.05 && voltageValue <= LINE_LEFT + 0.05)
-    {
-      rightFound = true;
-    }
-  }
-}
-int detectcolor()
-{
-  uint16_t clear, red, green, blue;
-  char couleur[1];
-  capteur.begin();
-  delay(250); // takes 50ms to read
-  capteur.getRawData(&red, &green, &blue, &clear);
-
-  if (red > 250 && green > 250 && blue > 100)
-  {
-    couleur[0] = 'j'; //test
-    Serial.print(" \ncouleur\t ");
-    Serial.print(couleur); //test
-    return 1;
-  }
-  else if (red > 200 && green > 100 && blue < green)
-  {
-    couleur[0] = 'r'; //test
-    Serial.print(" \ncouleur\t ");
-    Serial.print(couleur); //test
-    return 2;
-  }
-  else
-  {
-    couleur[0] = 'b'; //test
-    Serial.print(" \ncouleur\t ");
-    Serial.print(couleur); //test
-    return 3;
-  }
-  return 0;
-}
-
-void porterBalle()
-{
-
-  int couleur = detectColor();
-  delay(2000);
-  for (int i = 0; NBR_DISTANCE > i; i++)
-  {
-    executionStepParcours(tabDistance[i], tabAngle[i]);
-  }
-  delay(1500);
-  servoMoteur(100);
-  delay(1500);
-  if (couleur == 1)
-  {
-    for (int i = 0; NBR_DISTANCEj > i; i++)
-    {
-      executionStepParcours(tabDistancej[i], tabAnglej[i]);
-    }
-    delay(2500);
-    servoMoteur(150);
-    delay(1500);
-    for (int i = 0; NBR_DISTANCEjr > i; i++)
-    {
-      executionStepParcours(tabDistancejr[i], tabAnglejr[i]);
-    }
-  }
-  if (couleur == 2)
-  {
-    for (int i = 0; NBR_DISTANCEr > i; i++)
-    {
-      executionStepParcours(tabDistancer[i], tabAngler[i]);
-    }
-    delay(2500);
-    servoMoteur(150);
-    delay(1500);
-    for (int i = 0; NBR_DISTANCErr > i; i++)
-    {
-      executionStepParcours(tabDistancerr[i], tabAnglerr[i]);
-    }
-  }
-  if (couleur == 3)
-  {
-    for (int i = 0; NBR_DISTANCEb > i; i++)
-    {
-      executionStepParcours(tabDistanceb[i], tabAngleb[i]);
-    }
-    delay(2500);
-    servoMoteur(150);
-    delay(1500);
-    for (int i = 0; NBR_DISTANCEbr > i; i++)
-    {
-      executionStepParcours(tabDistancebr[i], tabAnglebr[i]);
-    }
-  }
-}
-void servoMoteur(int angle)
-{
-  if (angle > angleMoteur)
-  {
-    for (int i = angleMoteur; i < angle; i += 1)
-    {
-      SERVO_SetAngle(0, i);
-      SERVO_SetAngle(1, i);
-      delay(15);
-    }
-  }
-  else
-  {
-    for (int i = angleMoteur; i > angle; i -= 1)
-    {
-      SERVO_SetAngle(0, i);
-      SERVO_SetAngle(1, i);
-      delay(15);
-    }
-  }
-  angleMoteur = angle;
-}
-
-bool executionStepParcours(uint16_t distance, int angle)
-{
-
-  movingFowardRobot(distance);
-  delay(75);
-
-  turnedRobot(angle);
-  delay(75);
-
-  Serial.println(" ");
-
-  Serial.println(" ");
-  return 1;
 }
 
 void stopMotor()
 {
   MOTOR_SetSpeed(RIGHT_WHEEL, 0);
   MOTOR_SetSpeed(LEFT_WHEEL, 0);
-}
-
-void movingFowardRobot(uint16_t distance)
-{
-  // uint16_t distance (CENTIMÈTRE);
-  // bool sens (0 = à l'envers. 1 = à l'endroit);
-  distanceEncodeur = (distance / RESOLUTION_ENCODER);
-  readEncoder0 = 0;
-  readEncoder1 = 0;
-  motorLeft = 0;
-  motorRight = 0;
-  sumError = 0;
-
-  ENCODER_Reset(RIGHT_WHEEL);
-  ENCODER_Reset(LEFT_WHEEL);
-
-  while (((distanceEncodeur) > readEncoder0) && ((distanceEncodeur) > readEncoder1))
-  {
-
-    readEncoder0 = ENCODER_Read(LEFT_WHEEL);
-    readEncoder1 = ENCODER_Read(RIGHT_WHEEL);
-
-    float pourcentageVitesse = float(readEncoder0 / distanceEncodeur) * 100;
-
-    if (distance > 80)
-      motorLeft = accelerationDecelerationPID(pourcentageVitesse, ACCELERATION_HIGH_DISTANCE, DECCELERATION_HIGH_DISTANCE, SPEED_MAX_HIGH_DISTANCE, motorLeft);
-    else
-      motorLeft = accelerationDecelerationPID(pourcentageVitesse, ACCELERATION_LOW_DISTANCE, DECCELERATION_LOW_DISTANCE, SPEED_MAX_LOW_DISTANCE, motorLeft);
-
-    //PID FOR ENCODER
-    float error = readEncoder0 - readEncoder1;
-    float p = error * KP_FOWARD;
-
-    sumError += error;
-    float i = sumError * KI_FOWARD;
-
-    float adjSpeed = p + i;
-    motorRight = motorLeft + adjSpeed;
-
-    MOTOR_SetSpeed(LEFT_WHEEL, motorLeft);
-    MOTOR_SetSpeed(RIGHT_WHEEL, motorRight);
-  }
-
-  stopMotor();
-
-  Serial.println(distance);
-  Serial.println(distanceEncodeur);
-  Serial.println(readEncoder0);
-  Serial.println(readEncoder1);
-  Serial.println(" ");
 }
 
 void turnedRobot(int angle)
@@ -555,35 +257,61 @@ void turnedRobot(int angle)
 
   stopMotor();
 
-  Serial.println(angle);
+  /*Serial.println(angle);
   Serial.println(distanceEncodeur);
   Serial.println(readEncoder0);
   Serial.println(readEncoder1);
-  Serial.println(" ");
+  Serial.println(" ");*/
 }
 
-void reverseTab(int *tab, uint16_t sizeTab, bool sens)
+void movingFowardRobot(uint16_t distance)
 {
+  // uint16_t distance (CENTIMÈTRE);
+  // bool sens (0 = à l'envers. 1 = à l'endroit);
+  distanceEncodeur = (distance / RESOLUTION_ENCODER);
+  readEncoder0 = 0;
+  readEncoder1 = 0;
+  motorLeft = 0;
+  motorRight = 0;
+  sumError = 0;
 
-  if (sens)
+  ENCODER_Reset(RIGHT_WHEEL);
+  ENCODER_Reset(LEFT_WHEEL);
+
+  while (((distanceEncodeur) > readEncoder0) && ((distanceEncodeur) > readEncoder1))
   {
 
-    tmpValueTab = -1 * tab[sizeTab - 1];
+    readEncoder0 = ENCODER_Read(LEFT_WHEEL);
+    readEncoder1 = ENCODER_Read(RIGHT_WHEEL);
 
-    for (int i = sizeTab - 1; i > 0; i--)
-      tab[i] = -1 * tab[i - 1];
+    float pourcentageVitesse = float(readEncoder0 / distanceEncodeur) * 100;
 
-    tab[0] = tmpValueTab;
+    if (distance > 80)
+      motorLeft = accelerationDecelerationPID(pourcentageVitesse, ACCELERATION_HIGH_DISTANCE, DECCELERATION_HIGH_DISTANCE, SPEED_MAX_HIGH_DISTANCE, motorLeft);
+    else
+      motorLeft = accelerationDecelerationPID(pourcentageVitesse, ACCELERATION_LOW_DISTANCE, DECCELERATION_LOW_DISTANCE, SPEED_MAX_LOW_DISTANCE, motorLeft);
+
+    //PID FOR ENCODER
+    float error = readEncoder0 - readEncoder1;
+    float p = error * KP_FOWARD;
+
+    sumError += error;
+    float i = sumError * KI_FOWARD;
+
+    float adjSpeed = p + i;
+    motorRight = motorLeft + adjSpeed;
+
+    MOTOR_SetSpeed(LEFT_WHEEL, motorLeft);
+    MOTOR_SetSpeed(RIGHT_WHEEL, motorRight);
   }
-  else
-  {
-    tmpValueTab = -1 * tab[0];
 
-    for (uint16_t i = 0; i < sizeTab; i++)
-      tab[i] = -1 * tab[i + 1];
+  stopMotor();
 
-    tab[sizeTab - 1] = tmpValueTab;
-  }
+  /*Serial.println(distance);
+  Serial.println(distanceEncodeur);
+  Serial.println(readEncoder0);
+  Serial.println(readEncoder1);
+  Serial.println(" ");*/
 }
 
 float accelerationDecelerationPID(float pourcentageVitesse, uint8_t acceleration, uint8_t deceleration, float maxSpeed, float speed)
@@ -619,43 +347,67 @@ float accelerationDecelerationPID(float pourcentageVitesse, uint8_t acceleration
     return speed;
   }
 }
-void detectSound()
+
+int detectColor()
 {
-  float voltageValue = analogRead(ANALOG_BUZZER) * (5 / 1023.0);
-  Serial.println(voltageValue);
-  float target = 0.15;
-  if (voltageValue >= target)
+    uint16_t clear, red, green, blue;
+    char couleur[1];
+    capteur.begin();
+    delay(250); // takes 50ms to read
+    capteur.getRawData(&red, &green, &blue, &clear);
+
+    if (red > 500 && red < 600 && green > 525 && green < 625 && blue > 310 && blue < 410)
+    {
+      couleur[0] = 'j'; //test
+      Serial.print(" \ncouleur\t ");
+      Serial.print(couleur); //test
+      return 1;
+    }
+    else if (red > 450 && red < 550 && green > 375 && green < 475 && blue > 320 && blue < 430)
+    {
+      couleur[0] = 'r'; //test
+      Serial.print(" \ncouleur\t ");
+      Serial.print(couleur); //test
+      return 2;
+    }
+    else if (red > 350 && red < 450 && green > 460 && green < 560 && blue > 370 && blue < 470)
+    {
+      couleur[0] = 'b'; //test
+      Serial.print(" \ncouleur\t ");
+      Serial.print(couleur); //test
+      return 3;
+    }
+    Serial.print("C:\t");
+    Serial.print(clear);
+    Serial.print("\tR:\t");
+    Serial.print(red);
+    Serial.print("\tG:\t");
+    Serial.print(green);
+    Serial.print("\tB:");
+    Serial.print(blue);
+    Serial.println(" ");
+    return 0;
+}
+
+void servoMoteur(int angle)
+{
+  if (angle > angleMoteur)
   {
-    //lightLED(1, 1, 1, 1);
-    /* SOFT_TIMER_Disable(DETECT_SOUND_TIMER);
-    SOFT_TIMER_Enable(DETECT_BOWLING_PIN_TIMER); */
+    for (int i = angleMoteur; i < angle; i += 1)
+    {
+      SERVO_SetAngle(0, i);
+      SERVO_SetAngle(1, i);
+      delay(15);
+    }
   }
+  else
+  {
+    for (int i = angleMoteur; i > angle; i -= 1)
+    {
+      SERVO_SetAngle(0, i);
+      SERVO_SetAngle(1, i);
+      delay(15);
+    }
+  }
+  angleMoteur = angle;
 }
-
-void lightLED(bool red, bool green, bool blue, bool yellow)
-{
-  digitalWrite(DIGITAL_RED_LED, red);
-  digitalWrite(DIGITAL_GREEN_LED, green);
-  digitalWrite(DIGITAL_BLUE_LED, blue);
-  digitalWrite(DIGITAL_YELLOW_LED, yellow);
-}
-
-void initLEDsPin()
-{
-  pinMode(DIGITAL_RED_LED, OUTPUT);
-  pinMode(DIGITAL_GREEN_LED, OUTPUT);
-  pinMode(DIGITAL_BLUE_LED, OUTPUT);
-  pinMode(DIGITAL_YELLOW_LED, OUTPUT);
-}
-
-/* void init_Detection5kHz(){
-  PORTK |= (1<<7); //active PULL_DOWN A15
-  PCICR |= (1<<2); //enable PCINT 23
-  PCMSK2 |= (1<<7);
-  sei();
-}
-
-ISR(PCINT2_vect){
-  SOFT_TIMER_Disable(FOLLOW_LINE_TIMER);
-
-} */
