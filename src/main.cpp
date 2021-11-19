dbe1788c31d19ee7f0dbecc655344f4d2ee293a5
shawnmiller124 <83323320+shawnmiller124@users.noreply.github.com>
2021-11-18 22:22:19 -0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -5,9 +5,9 @@
  * @brief Programme du combattant
  * @version 0.2
  * @date 2021-11-01
- * 
+ *
  * @copyright Copyright (c) 2021
- * 
+ *
  */
 #include <Arduino.h>
 #include <LibRobus.h>
@@ -23,40 +23,38 @@
 #define WHEEL_BASE_SPEED 0.25
 #define WHEEL_ADD_SPEED 0.05
 
-
 #define TIMER_LINE_FALLOWER 3
 Adafruit_TCS34725 capteur = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_700MS, TCS34725_GAIN_1X); // création de l'objet capteur
 
 // OBP-704 possible values for suiveur de ligne
-#define LINE_CENTER 1.45   // 1 0 0
+#define LINE_CENTER 1.45 // 1 0 0
 #define LINE_RIGHT 2.80  // 0 0 1
-#define LINE_LEFT 0.79 // 0 1 0
+#define LINE_LEFT 0.79   // 0 1 0
 
 // ANALOGS
 #define ANALOG_LINE_FOLLOWER A0
 
 // DIGITALS
 
-
-//WHEEL
+// WHEEL
 #define ENCODER_STEP 3200
-#define WHEEL_DIAMETER 7.62 //cm
+#define WHEEL_DIAMETER 7.62 // cm
 #define WHEEL_CIRCUMFERENCE (WHEEL_DIAMETER * PI)
 #define RESOLUTION_ENCODER (WHEEL_CIRCUMFERENCE / ENCODER_STEP)
 #define ROBOT_CIRCUMFERENCE (19.05 * PI)
 #define RIGHT_WHEEL 1
 #define LEFT_WHEEL 0
 
-//FOWARD
+// FOWARD
 #define SPEED_MAX_HIGH_DISTANCE 0.6
 #define SPEED_MAX_LOW_DISTANCE 0.4
 #define SPEED_MIN 0.2
-#define ACCELERATION_HIGH_DISTANCE 20  //accelere jusqu'a 10% de la distance
-#define DECCELERATION_HIGH_DISTANCE 75 //deccelere a partir de 90% de la distance
-#define ACCELERATION_LOW_DISTANCE 20   //accelere jusqu'a 10% de la distance
-#define DECCELERATION_LOW_DISTANCE 75  //deccelere a partir de 90% de la distance
+#define ACCELERATION_HIGH_DISTANCE 20  // accelere jusqu'a 10% de la distance
+#define DECCELERATION_HIGH_DISTANCE 75 // deccelere a partir de 90% de la distance
+#define ACCELERATION_LOW_DISTANCE 20   // accelere jusqu'a 10% de la distance
+#define DECCELERATION_LOW_DISTANCE 75  // deccelere a partir de 90% de la distance
 
-//PID
+// PID
 #define KP_FOWARD 0.00330
 #define KI_FOWARD 0.000005
 #define KP_TURNING 0.00325
@@ -68,39 +66,55 @@ float readEncoder0;
 float readEncoder1;
 int distanceEncodeur;
 
-//MOTOR
+// MOTOR
 float motorLeft = 0;
 float motorRight = 0;
 
-//SERVOMOTOR
+// SERVOMOTOR
 int angleMoteur;
 
-//NRF24L01
+// NRF24L01
 RF24 radio(7, 8); // CE, CSN
 const byte address[6] = "00001";
-char text[32] = "";
+char readDataCarousel[30];
+char sendDataCarousel[] = "1";
 
 void NRF24L01_Init();
-void NRF24L01_TransmitData(char * data, uint8_t size);
-void NRF24L01_ReceiveData(char * data, uint8_t size);
+void NRF24L01_TransmitData(char *data, uint8_t size);
+bool turnCarousel();
 
-
+// step to follow
 typedef enum _step
 {
-
+  IDENTIFICATION,
+  GO_TO_CAROUSEL,
+  FIND_GOOD_KEY,
+  TAKE_KEY,
+  DROP_KEY,
+  DRUNK_KEY,
+  GO_BACK
 } _step;
 _step Step;
 
-//definition fonction
+// HID
+typedef enum _identification
+{
+  MENU,
+  DEPOT,
+  RETRAIT,
+  DEPOT_CLEF,
+} _identification;
+_identification Identification;
 
+// definition fonction
 void executeStep();
 
-//timer function
+// timer function
 void timerInit(uint8_t id, void (*func)(), unsigned long delay, int32_t nrep);
 void timerDetectionLineFallower();
 volatile float voltageValue;
 
-//motor function
+// motor function
 void stopMotor();
 void movingFowardRobot(uint16_t distance);
 void turnedRobot(int angle);
@@ -109,101 +123,80 @@ float accelerationDecelerationPID(float pourcentageVitesse, uint8_t acceleration
 void servoMoteur(int angle);
 int detectColor();
 
-
 void setup()
 {
   //BoardInit();
   Serial.begin(9600);
   NRF24L01_Init();
-  delay(2000);
 
-  //timerInit(TIMER_LINE_FALLOWER, timerDetectionLineFallower, 2, -1);
-
+  // timerInit(TIMER_LINE_FALLOWER, timerDetectionLineFallower, 2, -1);
 
   readEncoder0 = 0;
   readEncoder1 = 0;
-  //servoMoteur(150);
 
-  //Step = FALLOW_LINE1;
+  Step = IDENTIFICATION;
 
-  //SOFT_TIMER_Enable(TIMER_LINE_FALLOWER); //active suiveur de ligne
-
+  // SOFT_TIMER_Enable(TIMER_LINE_FALLOWER); //active suiveur de ligne
 }
 
 void loop()
 {
-  //SOFT_TIMER_Update();
+  // SOFT_TIMER_Update();
 
-  if (radio.available()) {
-    //NRF24L01_ReceiveData(text, sizeof(text));
-    radio.read(&text, sizeof(text));
-    Serial.println(text);
-  }
+  bool value = turnCarousel();
+  Serial.println(value);
 
-  //executeStep();
+  if (value)
+    Serial.println("fonctionne!!!");
+  else
+    Serial.println("fonctionne pas...");
+
+  delay(1000);
+  // executeStep();
 }
 
 void executeStep()
 {
 
-  /*switch (Step)
+  switch (Step)
   {
-  case FALLOW_LINE1:
+  case IDENTIFICATION:
 
+    Step = GO_TO_CAROUSEL;
     break;
 
-  case FIND_PIN:
+  case GO_TO_CAROUSEL:
 
+    Step = FIND_GOOD_KEY;
     break;
 
-  case TURN_TO_PIN:
+  case FIND_GOOD_KEY:
 
+    // Step = TAKE_KEY;
+    // Step = DROP_KEY;
+    // Step = DRUNK_KEY;
     break;
 
-  case GO_TO_PIN:
+  case TAKE_KEY:
 
+    Step = GO_BACK;
     break;
 
-  case TURN_RIGHT:
+  case DROP_KEY:
 
+    Step = GO_BACK;
     break;
 
-  case FALLOW_LINE_COLOR:
+  case DRUNK_KEY:
 
+    Step = GO_BACK;
     break;
 
-  case TURN_LEFT:
+  case GO_BACK:
 
+    Step = IDENTIFICATION;
     break;
-
-  case FALLOW_LINE2:
-
-    break;
-
-  case DETECT_COLOR:
-
-    break;
-
-  case PICKUP_BALL:
-
-    break;
-
-  case RED_PATH:
-
-    break;
-
-  case YELLOW_PATH:
-
-    break;
-
-  case BLUE_PATH:
-
-    break;
-
-  case FIND_WHITE_LINE:
-
-    break;
-  }*/
+  }
 }
 
 void timerInit(uint8_t id, void (*func)(), unsigned long delay, int32_t nrep)
@@ -217,7 +210,7 @@ void timerInit(uint8_t id, void (*func)(), unsigned long delay, int32_t nrep)
 void timerDetectionLineFallower()
 {
   voltageValue = (analogRead(ANALOG_LINE_FOLLOWER)) * (5 / 1023.0);
-  //Serial.println(voltageValue);
+  // Serial.println(voltageValue);
 }
 
 void stopMotor()
@@ -246,11 +239,11 @@ void turnedRobot(int angle)
   ENCODER_Reset(LEFT_WHEEL);
 
   while (((distanceEncodeur) > abs(readEncoder0)) && ((distanceEncodeur) > abs(readEncoder1)))
-  { //les 2 roue tournes
+  { // les 2 roue tournes
 
     motorLeft = SPEED_MIN;
 
-    //PID FOR ENCODER
+    // PID FOR ENCODER
     float error = abs(readEncoder0) - abs(readEncoder1);
     float p = error * KP_TURNING;
 
@@ -261,11 +254,11 @@ void turnedRobot(int angle)
     motorRight = motorLeft + adjSpeed;
 
     if (angle > 0)
-    { //test si l'angle est negative, tourne vers la droite
+    { // test si l'angle est negative, tourne vers la droite
       motorLeft = -1 * motorLeft;
     }
     else if (angle < 0)
-    { //test si l'angle est positive, tourne vers la gauche
+    { // test si l'angle est positive, tourne vers la gauche
       motorRight = -1 * motorRight;
     }
 
@@ -312,7 +305,7 @@ void movingFowardRobot(uint16_t distance)
     else
       motorLeft = accelerationDecelerationPID(pourcentageVitesse, ACCELERATION_LOW_DISTANCE, DECCELERATION_LOW_DISTANCE, SPEED_MAX_LOW_DISTANCE, motorLeft);
 
-    //PID FOR ENCODER
+    // PID FOR ENCODER
     float error = readEncoder0 - readEncoder1;
     float p = error * KP_FOWARD;
 
@@ -341,7 +334,7 @@ float accelerationDecelerationPID(float pourcentageVitesse, uint8_t acceleration
   float _speed = 0;
   float dAcceleration = maxSpeed / acceleration;
   float dDeceleration = -maxSpeed / (100 - deceleration);
-  float b = maxSpeed - (dDeceleration * deceleration); //pente de la fonction de decrementation
+  float b = maxSpeed - (dDeceleration * deceleration); // pente de la fonction de decrementation
 
   if (acceleration >= pourcentageVitesse)
   { // de 0 a ACCELERATION%
@@ -371,43 +364,43 @@ float accelerationDecelerationPID(float pourcentageVitesse, uint8_t acceleration
 
 int detectColor()
 {
-    uint16_t clear, red, green, blue;
-    char couleur[1];
-    capteur.begin();
-    delay(250); // takes 50ms to read
-    capteur.getRawData(&red, &green, &blue, &clear);
+  uint16_t clear, red, green, blue;
+  char couleur[1];
+  capteur.begin();
+  delay(250); // takes 50ms to read
+  capteur.getRawData(&red, &green, &blue, &clear);
 
-    if (red > 500 && red < 600 && green > 525 && green < 625 && blue > 310 && blue < 410)
-    {
-      couleur[0] = 'j'; //test
-      Serial.print(" \ncouleur\t ");
-      Serial.print(couleur); //test
-      return 1;
-    }
-    else if (red > 450 && red < 550 && green > 375 && green < 475 && blue > 320 && blue < 430)
-    {
-      couleur[0] = 'r'; //test
-      Serial.print(" \ncouleur\t ");
-      Serial.print(couleur); //test
-      return 2;
-    }
-    else if (red > 350 && red < 450 && green > 460 && green < 560 && blue > 370 && blue < 470)
-    {
-      couleur[0] = 'b'; //test
-      Serial.print(" \ncouleur\t ");
-      Serial.print(couleur); //test
-      return 3;
-    }
-    Serial.print("C:\t");
-    Serial.print(clear);
-    Serial.print("\tR:\t");
-    Serial.print(red);
-    Serial.print("\tG:\t");
-    Serial.print(green);
-    Serial.print("\tB:");
-    Serial.print(blue);
-    Serial.println(" ");
-    return 0;
+  if (red > 500 && red < 600 && green > 525 && green < 625 && blue > 310 && blue < 410)
+  {
+    couleur[0] = 'j'; // test
+    Serial.print(" \ncouleur\t ");
+    Serial.print(couleur); // test
+    return 1;
+  }
+  else if (red > 450 && red < 550 && green > 375 && green < 475 && blue > 320 && blue < 430)
+  {
+    couleur[0] = 'r'; // test
+    Serial.print(" \ncouleur\t ");
+    Serial.print(couleur); // test
+    return 2;
+  }
+  else if (red > 350 && red < 450 && green > 460 && green < 560 && blue > 370 && blue < 470)
+  {
+    couleur[0] = 'b'; // test
+    Serial.print(" \ncouleur\t ");
+    Serial.print(couleur); // test
+    return 3;
+  }
+  Serial.print("C:\t");
+  Serial.print(clear);
+  Serial.print("\tR:\t");
+  Serial.print(red);
+  Serial.print("\tG:\t");
+  Serial.print(green);
+  Serial.print("\tB:");
+  Serial.print(blue);
+  Serial.println(" ");
+  return 0;
 }
 
 void servoMoteur(int angle)
@@ -433,32 +426,46 @@ void servoMoteur(int angle)
   angleMoteur = angle;
 }
 
-void NRF24L01_Init(){
-
+void NRF24L01_Init()
+{
   radio.begin();
-  //receiver
+  // receiver
   radio.openReadingPipe(0, address);
 
-  //transmitter
+  // transmitter
   radio.openWritingPipe(address);
-
   radio.setPALevel(RF24_PA_MIN);
+}
+
+void NRF24L01_TransmitData(char *data, uint8_t size)
+{
+  radio.stopListening();
+
+  radio.write(data, size);
+  //Serial.println(data);
 
   radio.startListening();
 }
 
-void NRF24L01_TransmitData(char *data, uint8_t size){
-  radio.stopListening(); 
+bool turnCarousel()
+{
 
-  radio.write(&data, size);
+  NRF24L01_TransmitData(sendDataCarousel, sizeof(sendDataCarousel));
 
-  delay(500);
+  int cpt = 0;
+  while (!radio.available()){ // attent le retour de commande carousel
+    delay(1);
+    cpt++;
+    if(cpt >= 1000)
+      return 0;
+  } 
 
-  radio.startListening();
+  radio.read(&readDataCarousel, 1);
+  Serial.println(readDataCarousel);
+  radio.flush_rx();
+
+  if (readDataCarousel[0] == '1')
+    return 1;
+  else
+    return 0;
 }
-
-void NRF24L01_ReceiveData(char *data, uint8_t size){
-
-  radio.read(&data, size);
-  
-  }
